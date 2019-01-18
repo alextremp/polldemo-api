4630f66b68986b1b964c3181bdea85228aaea88b
alextremp <alex.castells@schibsted.com>
2019-01-18 12:23:50 +0100

    fix all tests execution

--- src/test/java/com/schibsted/onepunch/polldemo/integration/AbstractIntegrationTestCase.java
+++ src/test/java/com/schibsted/onepunch/polldemo/integration/AbstractIntegrationTestCase.java
@@ -1,5 +1,8 @@
 package com.schibsted.onepunch.polldemo.integration;
 
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,4 +15,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ActiveProfiles("dev")
 public abstract class AbstractIntegrationTestCase {
 
+    @ClassRule
+    public static DockerComposeRule docker = DockerComposeRule.builder()
+            .file("docker-compose.yml")
+            .waitingForService("db", HealthChecks.toHaveAllPortsOpen())
+            .saveLogsTo("target/docker-logs")
+            .build();
 }
